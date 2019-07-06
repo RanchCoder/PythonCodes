50035403f6eea7b9210072e1ea62c80d500a2784
RanchCoder <52583285+RanchCoder@users.noreply.github.com>
2019-07-06 15:19:13 +0530

    Add files via upload

---
+++ AlienInterrogation(input()).py
@@ -0,0 +1,9 @@
+
+print('please enter one liners :: ',end='\n')
+name = input('Who are you ?')
+purpose = input('Why have you come here')
+source = input('Where do you belong to ');
+
+print('My name is ',name , sep=' ')
+print('I have come here to ',purpose ,sep = ' ')
+print('I belong to the ',source,sep=' ')
